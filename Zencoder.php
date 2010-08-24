88995d3be1dea9455f9a15f3f6681bc44d351cbf
Jim Puls <puls@pbworks.com>
2010-08-24 21:14:38 +0000

    This function_exists should be a method_exists

--- Zencoder.php
+++ Zencoder.php
@@ -62,7 +62,7 @@ class ZencoderJob {
       // Create output file objects
       if($attr_name == "outputs" && is_array($attr_value)) {
         $this->create_outputs($attr_value);
-      } elseif (!function_exists($this->$attr_name)) {
+      } elseif (!method_exists($this, $attr_name)) {
         $this->$attr_name = $attr_value;
       }
     }
